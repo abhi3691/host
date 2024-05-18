b550d81bf3bf49ac89131394d86ab212fc3ba1f7
abhinandvk <abhinandvk41@gmail.com>
2024-05-18 22:58:38 +0530

    updated

---
+++ containers.d.ts
@@ -0,0 +1,4 @@
+declare module 'app1/App' {
+  const Text: React.ComponentType;
+  export default Text;
+}
--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import {AppRegistry, Platform} from 'react-native';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {ScriptManager, Script, Federated} from '@callstack/repack/client';
 import {name as appName} from './app.json';
+import App from './App';
+
+const resolveURL = Federated.createURLResolver({
+  containers: {
+    app1: 'http://localhost:9000/[name][ext]',
+  },
+});
+
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+  let url;
+  if (caller === 'main') {
+    url = Script.getDevServerURL(scriptId);
+  } else {
+    url = resolveURL(scriptId, caller);
+  }
+
+  if (!url) {
+    return undefined;
+  }
+
+  return {
+    url,
+    cache: false, // For development
+    query: {
+      platform: Platform.OS,
+    },
+  };
+});
 
 AppRegistry.registerComponent(appName, () => App);
